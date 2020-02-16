26649bebb7b50fe4611902980a26415943751501
Tirta Rachman <tirtawening@gmail.com>
2020-02-16 13:11:40 -0500

    Update player UI

--- public/player/playerSketch.js
+++ public/player/playerSketch.js
@@ -1,10 +1,50 @@
-let socket = io('/player');
+$(document).ready(function () {
+    let socket = io('/player')
 
-// Listen for confirmation of connection
-socket.on('connect', function () {
-    console.log("Connected");
+    
+
+    // Listen for confirmation of connection
+    socket.on('connect', function () {
+        console.log("Connected")
+    })
+
+    let currentDisplayState = 'CHOOSE_TEAM'
+    updateDisplay()
+
+    register = function (teamNumber) {
+        socket.emit('register', { team: teamNumber })
+        currentDisplayState = 'SUBMIT_NUMBER'
+        updateDisplay()
+    }
+
+    submit = function(submittedNumber) {
+        socket.emit('submit', { submittedNumber: submittedNumber })
+        currentDisplayState = 'IDLE'
+        updateDisplay()
+    }
+
+    function updateDisplay() {
+        hideAllDivs()
+        switch (currentDisplayState) {
+            case 'CHOOSE_TEAM':
+                $("#login-buttons").show()
+                break;
+            case 'SUBMIT_NUMBER':
+                $("#keypad").show()
+                break;
+            case 'IDLE':
+                $("#splash-text").show()
+                break;
+            default:
+                break;
+        }
+    }
+
+    function hideAllDivs() {
+        $("#login-buttons").hide()
+        $("#keypad").hide()
+        $("#splash-text").hide()
+    }
+
 });
 
-register = function (teamNumber) {
-    socket.emit('register', { team: teamNumber });
-}
