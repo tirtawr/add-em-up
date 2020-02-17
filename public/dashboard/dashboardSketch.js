ade22cc1dfcfae94dec44e54790b4e166dd506bc
Tirta Rachman <tirtawening@gmail.com>
2020-02-17 13:17:34 -0500

    Add points submission on dashboard

--- public/dashboard/dashboardSketch.js
+++ public/dashboard/dashboardSketch.js
@@ -13,6 +13,10 @@ $(document).ready(function () {
     }
 
     function updateDisplayWithStatus(status) {
+        const currentRound = status['current_round']
+        const currentTurnInRound = status['current_turn_in_round']
+        const currentRoundTurnStart = (currentRound - 1) * 3
+
         // Update round target
         if (status['current_stage'] == 'END_GAME') {
             $(`#current-target`).html('Thanks For Playing!')
@@ -31,11 +35,30 @@ $(document).ready(function () {
             $(`#point-text-${i}`).html(getStars(status['scores'][`team_${i}`]))
         }
 
-        // Update team running su,
+        // Update team running sum
         for (let i = 1; i <= 3; i++) {
             $(`#running-total-text-${i}`).html(
-                (status['running_sum_for_current_round'][`team_${i}`])
+                status['running_sum_for_current_round'][`team_${i}`]
             )
+        }
+
+        // Update submissions
+        for (let teamIndex = 1; teamIndex <= 3; teamIndex++) {
+            for (let turnIndex = 1; turnIndex <= 3; turnIndex++) {
+                for (let playerIndex = 1; playerIndex <= 4; playerIndex++) {
+                    const totalTurnIndex = currentRoundTurnStart + turnIndex - 1
+                    if (status['players'][`team_${teamIndex}`][playerIndex-1] && status['players'][`team_${teamIndex}`][playerIndex-1]['submissions'][totalTurnIndex] > 0) {
+                        $(`#submission-text-${teamIndex}-${turnIndex}-${playerIndex}`).html(
+                            status['players'][`team_${teamIndex}`][playerIndex-1]['submissions'][totalTurnIndex]
+                        )
+                    } else {
+                        $(`#submission-text-${teamIndex}-${turnIndex}-${playerIndex}`).html(
+                            '?'
+                        )
+                    }
+                    
+                }
+            }   
         }
     }
 
